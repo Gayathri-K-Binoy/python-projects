bfd1e9a43a4709e287f865b2f203aacba8c9eb66
Gayathri <gayathrikbinoy@gmail.com>
2026-06-16 00:19:28 +0530

    added functions

---
+++ 0-BASICS/6-functions.py
@@ -0,0 +1,26 @@
+#functions are blocks of code that can be reused. they take arguments and return values
+# def greet_everyone():
+#     print("Hello everyone")
+
+# greet_everyone()
+
+# def greet(name):
+#     print("Hello,",name)
+# greet("Helem")
+# greet("Disk")
+# greet("Hima")
+
+#with functions
+def greet_user(name):
+    print("Hello,",name,"Welcome to our app")
+    print("We hope you enjoy using app\n Let us know if you need help")
+
+greet_user('Bina')
+greet_user("Dila")
+
+def power(base,exp):
+    return base**exp
+
+print(power(2,4))
+print(power(12,2))
+print(power(4,3))
